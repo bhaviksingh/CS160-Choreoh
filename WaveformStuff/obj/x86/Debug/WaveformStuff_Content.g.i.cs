3b17d8b09b001ae14555b0c7ad36c263802f60d4
Samuel Zhu <sjzhu@berkeley.edu>
2012-04-07 22:40:35 -0700

    Added start/stop sliders

--- WaveformStuff/obj/x86/Debug/WaveformStuff_Content.g.i.cs
+++ WaveformStuff/obj/x86/Debug/WaveformStuff_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("beatit-waveform.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("stopslider.png")]
 
 
