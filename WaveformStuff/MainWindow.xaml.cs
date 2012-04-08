3c753f9d2e0bae1fe1bcf778c251a1240034649c
Samuel Zhu <sjzhu@berkeley.edu>
2012-04-07 20:39:45 -0700

    Added selection rectangle and movement checking

--- WaveformStuff/MainWindow.xaml.cs
+++ WaveformStuff/MainWindow.xaml.cs
@@ -20,10 +20,14 @@ namespace WaveformStuff
     public partial class MainWindow : Window
     {
         private Canvas wfCanvas;
+        private bool select;
+        private Rectangle selectRect;
         public MainWindow()
         {
             InitializeComponent();
             wfCanvas = new Canvas();
+            select = false;
+            selectRect = null;
         }
         /**
          * Takes in the width of a waveform image, the length of a song in seconds, a properly formatted string for an image, and the Canvas this all needs to be drawn in.
@@ -47,7 +51,7 @@ namespace WaveformStuff
                 Stretch = Stretch.Fill
             };
             waveformCanvas.Children.Add(waveformImage);  //adds and positions waveformImage within waveformCanvas
-            Canvas.SetLeft(waveformImage, 0);
+            Canvas.SetLeft(waveformImage, 8);
             Canvas.SetTop(waveformImage, 0);
             waveformImage.Source = new BitmapImage(new Uri(source,UriKind.RelativeOrAbsolute)); //adds source to waveformImage
             double timeInc = 30 * ((double) width) / ((double) seconds);
@@ -77,6 +81,49 @@ namespace WaveformStuff
             return waveformCanvas;
         }
 
+        public Rectangle selectSegment(int width, int seconds, int start, int length, Canvas waveformCanvas, Rectangle selection)
+        {
+            double timeInc = ((double)width) / ((double)seconds);
+            if (selection == null)
+            {
+                selection = new Rectangle
+                {
+                    Height = waveformCanvas.Height,
+                    Width = timeInc,
+                    Visibility = Visibility.Hidden,
+                    Opacity = .2,
+                    Fill = Brushes.Orange,
+                    Stroke = Brushes.Transparent
+                };
+                waveformCanvas.Children.Add(selection);
+            }
+            Canvas.SetLeft(selection, start * timeInc + 8);
+            Canvas.SetTop(selection, 0);
+            selection.Width = length * timeInc;
+            selection.Visibility = Visibility.Visible;
+            return selection;
+        }
+
+        public Rectangle deselectSegment(int width, int seconds, int start, int length, Canvas waveformCanvas, Rectangle selection)
+        {
+            double timeInc = ((double)width) / ((double)seconds);
+            if (selection == null)
+            {
+                selection = new Rectangle
+                {
+                    Height = waveformCanvas.Height,
+                    Width = timeInc,
+                    Visibility = Visibility.Hidden,
+                    Opacity = .5,
+                    Fill = Brushes.Orange,
+                    Stroke = Brushes.Transparent
+                };
+                waveformCanvas.Children.Add(selection);
+            }
+            selection.Visibility = Visibility.Hidden;
+            return selection;
+        }
+
         private string numToTime(int num) // helper function to convert a time in seconds to a 'min:sec' format
         {
             int min = 0;
@@ -95,12 +142,42 @@ namespace WaveformStuff
         }
 
         /**
-         * Will shift given canvas the given amount of pixels.
-         * Meant to be called every frame refresh in the overall application.
+         * Will incrementally shift given canvas the given amount of pixels.
+         * Uses waveformOnEdge to check to see if canvas is on edge of screen, does not move waveform if it is.
          **/
         public void shiftCanvas(int pixels, Canvas canvas)
         {
-            Canvas.SetLeft(canvas, Canvas.GetLeft(canvas) + pixels);
+            int inc = 1;
+            if (pixels < 0)
+            {
+                inc *= -1;
+                pixels *= -1;
+            }
+            for (int i = 0; i < pixels; i++)
+            {
+                if (waveformOnEdge(canvas) == 0 || ((waveformOnEdge(canvas) == -1) && inc < 0) || ((waveformOnEdge(canvas) == 1) && inc > 0))
+                {
+                    //textBlock1.Text = "" + waveformOnEdge(canvas);
+                    Canvas.SetLeft(canvas, Canvas.GetLeft(canvas) + inc);
+                    
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        /**
+         * Checks to see if waveform is on edge of screen (currently hardcoded to 1024).
+         * 
+         **/
+        public int waveformOnEdge(Canvas waveformCanvas)
+        {
+            textBlock1.Text = "" + (Canvas.GetLeft(waveformCanvas) + waveformCanvas.Width);
+            if(Canvas.GetLeft(waveformCanvas) == 0) return -1;
+            if(Canvas.GetLeft(waveformCanvas)+waveformCanvas.Width == 1024) return 1;
+            return  0;
         }
 
         //testing button functions follow
@@ -112,13 +189,20 @@ namespace WaveformStuff
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            int loop = Convert.ToInt32(pixelLoop.Text);
-            int shift = Convert.ToInt32(pixelShift.Text);
-            for(int i = 0; i < loop; i++)
+            shiftCanvas(Convert.ToInt32(pixelShift.Text), wfCanvas);
+        }
+
+        private void selectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (select)
             {
-                shiftCanvas(shift, wfCanvas);
-                shift*=-1;
+                selectRect = deselectSegment(1800, 259, Convert.ToInt32(segStart.Text), Convert.ToInt32(segLength.Text), wfCanvas, selectRect);
             }
+            else
+            {
+                selectRect = selectSegment(1800, 259, Convert.ToInt32(segStart.Text), Convert.ToInt32(segLength.Text), wfCanvas, selectRect);
+            }
+            select = !select;
         }
 
         
